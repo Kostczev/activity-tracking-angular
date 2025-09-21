dc669666eaf8b2aec7734980971f5940dbc82dba
Kostczev <churczev.k@gmail.com>
2025-09-21 21:32:18 +0500

    feat: add routing and page components structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { MonitoringComponent } from './pages/monitoring/monitoring';
+import { StatisticsComponent } from './pages/statistics/statistics';
+import { EditComponent } from './pages/edit/edit';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/monitoring', pathMatch: 'full' },
+  { path: 'monitoring', component: MonitoringComponent },
+  { path: 'statistics', component: StatisticsComponent },
+  { path: 'edit', component: EditComponent },
+  { path: '**', redirectTo: '/monitoring' }
+];
