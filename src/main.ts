8e20f0ae80d75d48717da7461eb9468f1c0270fb
Kostczev <churczev.k@gmail.com>
2025-09-24 20:34:24 +0500

    fix: an attempt to launch a web page v9

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-const isGitHubPages = window.location.hostname.includes('github.io');
-const baseHref = isGitHubPages ? '/activity-tracking-angular/' : '/';
+// const isGitHubPages = window.location.hostname.includes('github.io');
+// const baseHref = isGitHubPages ? '/activity-tracking-angular/' : '/';
 
-const baseElement = document.createElement('base');
-baseElement.href = baseHref;
-document.head.prepend(baseElement);
+// const baseElement = document.createElement('base');
+// baseElement.href = baseHref;
+// document.head.prepend(baseElement);
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
